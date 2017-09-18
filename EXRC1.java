32c6b46fed5f536b8f74119caedcb15063e6281d
Matheus Henrique <31678376+HonoredPrince@users.noreply.github.com>
2017-09-18 14:22:14 -0300

    Update and rename SMPSEQ3.java to EXRC1.java

---
+++ EXRC1.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class EXRC1{
+
+    public static void main(String[] args) {
+        try{    
+            int tmnS,tmnQ;
+
+            Scanner scan = new Scanner(System.in);
+
+            tmnS = sc.nextInt();
+
+            int[] S = new int[tmnS];
+
+            for(int i = 0; i < tmnS; ++i){
+                
+                S[i] = scan.nextInt();
+            }
+
+            tmnQ = scan.nextInt();
+
+            int[] Q = new int[tmnQ];
+
+            for(int i = 0; i < tmnQ; ++i){
+                
+                Q[i] = scan.nextInt();
+            }
+            boolean p;
+
+            int[] O = new int[tmnS];
+            int output = 0;
+
+            for(int i = 0; i < tmnS; ++i){
+
+                p = false;
+
+                for(int j = 0; j < tmnQ; ++j){
+                   if(S[i] == Q[j]){
+                     p = true;
+                     break;
+                   }
+                }
+                if(!p){
+                    O[output++] = S[i];
+                }
+            }
+
+            for(int i = 0; i < output; i++)
+            {
+                System.out.print(O[i]);
+                
+                if(i != output-1)
+                    System.out.print(" ");
+            }
+        }catch (Exception ex){}
+    }
+}
